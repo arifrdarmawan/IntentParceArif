6013985b5a10edd4cd0a25372ea381145d3da691
Arif Rachmat Darmawan <arifrdarmawan11@gmail.com>
2019-08-14 10:52:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentparcelable/MoveWithObjectActivity.java
@@ -0,0 +1,21 @@
+package com.example.intentparcelable;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveWithObjectActivity extends AppCompatActivity {
+    public static String EXTRA_PERSON = " extra_person";
+    public TextView tvObject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_object);
+        tvObject = (TextView)findViewById(R.id.tv_object_received);
+        Person mPerson = getIntent().getParcelableExtra(EXTRA_PERSON);
+        String text = "Nama : "+mPerson.getName()+", Email : "+mPerson.getEmail()+", Umur : "+mPerson.getAge()+", Lokasi : "+mPerson.getCity();
+        tvObject.setText(text);
+    }
+}
